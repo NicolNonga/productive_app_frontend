e2bc6da55abba559574a1b652fe404b5e4fe9b1c
NicolNonga <nicodemosarmindo@gmail.com>
2024-07-28 17:32:26 +0100

    feature: loading or refresh value from server

--- src/app/features/feature-logs/componets/logs/logs.component.ts
+++ src/app/features/feature-logs/componets/logs/logs.component.ts
@@ -1,19 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Signal } from '@angular/core';
+import { Component, EnvironmentInjector, inject, runInInjectionContext, Signal } from '@angular/core';
 import { ResponseLogs } from '../../models/logs.model';
 import { LogsServiceHttp } from '../../services/http-services/logs-service.http';
+import { TotalDesboardComponent } from '../../../dashboard-main/components/total-desboard/total-desboard.component';
+import { ResponseDashboardLogInterface } from '../../../dashboard-main/models/dashboard-logs.model';
+import { LogDashboardHttpService } from '../../../dashboard-main/services/http-service/log-dashboard.service.http';
 
 @Component({
   selector: 'app-logs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TotalDesboardComponent],
   templateUrl: './logs.component.html',
   styleUrl: './logs.component.css'
 })
 export class LogsComponent {
   public logs: Signal <ResponseLogs>
   public logHttpService = inject(LogsServiceHttp)
+  public dashboardLogsService = inject(LogDashboardHttpService)
+  public dasboard! : Signal <ResponseDashboardLogInterface>
+  public environmentInjector = inject(EnvironmentInjector)
   constructor(){
       this.logs = this.logHttpService.toSignalLogs()
+      this.dasboard = this.dashboardLogsService.toSignalDashboardLogs()
+  }
+
+  public realoadLogs(){
+    runInInjectionContext(this.environmentInjector, ()=> {
+      this.logs = this.logHttpService.toSignalLogs();
+      this.dasboard= this.dashboardLogsService.toSignalDashboardLogs()
+    })
   }
 }
