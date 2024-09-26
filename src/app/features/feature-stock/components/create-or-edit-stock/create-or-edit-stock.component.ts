61dfd79a4b98b206e0eeeec3081f0e7fff157419
NicolNonga <nicodemosarmindo@gmail.com>
2024-09-26 20:48:47 +0100

    feature: add stocke

--- src/app/features/feature-stock/components/create-or-edit-stock/create-or-edit-stock.component.ts
+++ src/app/features/feature-stock/components/create-or-edit-stock/create-or-edit-stock.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, Signal } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { StockForm } from '../../model/stock.model';
+import { PartHttpService } from '../../../feature-parts/service/part-http.service';
+import { Part } from '../../../feature-parts/model/part.model';
+import { Supplier } from '../../../feature-configuration/model/supplier.model';
+import { SupplierHttpService } from '../../../feature-configuration/services/supplier-http.service';
 
 @Component({
   selector: 'app-create-or-edit-stock',
@@ -8,5 +14,23 @@ import { Component } from '@angular/core';
   styleUrl: './create-or-edit-stock.component.css'
 })
 export class CreateOrEditStockComponent {
+    public isSubmitted: boolean = false
+    public stockForm!:FormGroup
+    public partHttpService= inject(PartHttpService)
+    public supplierService = inject(SupplierHttpService)
+    public parts: Signal<Part [] | undefined> = signal<Part []>([])
+    public suppliers: Signal<Supplier [] | undefined>= signal<Supplier []>([])
 
+     constructor(){
+        this.suppliers = this.supplierService.toSignaSupplierAll()
+        this.parts = this.partHttpService.toSignalPartAll()
+     }
+
+    createForm(){
+        this.stockForm = new FormGroup<StockForm>({
+            partId : new FormControl(null, Validators.required),
+            supplieId : new FormControl(null,Validators.required),
+            quantity: new FormControl(null, Validators.required)
+        })
+    }
 }
